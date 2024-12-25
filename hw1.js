ffc5e83fbac6fd9b460a0d76facf84517843c6a9
Maksim Kovalevski <maksimkovalevski@gmail.com>
2024-12-25 16:04:06 +0200

    test 1 first commit

---
+++ hw1.js
@@ -0,0 +1,12 @@
+/*вывод трех цветов*/
+console.log("Синий");
+console.log("Зеленый");
+console.log("Красный");
+/*вывод ромба*/
+console.log("   *   ");
+console.log("  ***  ");
+console.log(" ***** ");
+console.log("*******");
+console.log(" ***** ");
+console.log("  ***  ");
+console.log("   *   ");
